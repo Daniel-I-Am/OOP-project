3a8f324bf5208a86c2aa380e997f6a6852c10465
Daniel de Cloet <djdecloet@gmail.com>
2018-12-11 10:42:13 +0100

    Proper references

---
+++ src/Game.ts
@@ -0,0 +1,8 @@
+///<reference path="helper/CanvasHelper.ts"/>
+///<reference path="helper/KeyHelper.ts"/>
+///<reference path="helper/MathHelper.ts"/>
+///<reference path="dataObjects/Rotation.ts"/>
+///<reference path="dataObjects/Vector.ts"/>
+///<reference path="baseObjects/BaseView.ts"/>
+///<reference path="baseObjects/Entity.ts"/>
+///<reference path="entity/Enemy.ts"/>
