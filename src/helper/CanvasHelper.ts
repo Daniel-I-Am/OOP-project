6400757616c008da3080afed890967ab6efc4275
Daniel de Cloet <djdecloet@gmail.com>
2018-12-11 09:46:26 +0100

    set canvas size

--- src/helper/CanvasHelper.ts
+++ src/helper/CanvasHelper.ts
@@ -8,6 +8,9 @@ class CanvasHelper {
     ) {
         this.canvas = <HTMLCanvasElement>canvas;
         this.ctx = this.canvas.getContext('2d');
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
     }
 
     public Instance(canvas: HTMLElement = null): CanvasHelper {
