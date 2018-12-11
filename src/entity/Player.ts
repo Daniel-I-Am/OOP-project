f859c8accd8e1113c7b3ba8303f2c5edb868ac24
RubenSmn <voetbalruben@gmail.com>
2018-12-11 12:07:13 +0100

    added basic player class

---
+++ src/entity/Player.ts
@@ -0,0 +1,46 @@
+class Player extends Entity {
+    private keyHelper: KeyHelper;
+
+    /**
+     * @constructor
+     * @param {HTMLCanvasElement} canvas
+     * @param {string} imageSource
+     * @param {number} xPos
+     * @param {number} yPos
+     * @param {number} height
+     * @param {number} width
+     * @param {number} speed
+     */
+    constructor(
+        canvas: HTMLCanvasElement,
+        imageSource: string,
+        xPos: number,
+        yPos: number,
+        height: number,
+        width: number,
+        speed: number
+    ) {
+        super(
+            imageSource,
+            new Vector(xPos, yPos),
+            new Rotation(0),
+            new Vector(width, height),
+            speed
+        );
+
+        this.keyHelper = new KeyHelper();
+    }
+
+
+    public move(): void {
+        let x: number;
+        if (this.keyHelper.getLeftPressed())
+            x = this.location.getValue(0);
+            x -= this.speed;
+            this.location.updateValue(0, x);
+        if (this.keyHelper.getRightPressed())
+            x = this.location.getValue(0);
+            x += this.speed;
+            this.location.updateValue(0, x);
+    }
+}
