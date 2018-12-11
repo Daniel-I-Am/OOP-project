b7c5d6b3c2db32a41343f871bce1c44df50eab66
Daniel de Cloet <djdecloet@gmail.com>
2018-12-11 13:18:22 +0100

    draw player

--- src/views/GameView.ts
+++ src/views/GameView.ts
@@ -1,10 +1,21 @@
 class GameView extends BaseView {
+    private player: Player;
 
     public constructor() {
         super();
+        this.player = new Player([
+            "./assets/player/anim_walk/PlayerAnim1.png",
+            "./assets/player/anim_walk/PlayerAnim2.png",
+            "./assets/player/anim_walk/PlayerAnim1.png",
+            "./assets/player/anim_walk/PlayerAnim3.png",
+        ],
+        this.canvasHelper.getCenter(),
+        new Vector(312, 800), 1, 5
+        );
     }
 
     public update(): void {
+        this.player.update();
         this.drawGUI();
     }
 
