89991e09f05f516b47f6c8cce9bfc8439b473e5d
Daniel de Cloet <djdecloet@gmail.com>
2018-12-17 10:34:26 +0100

    Drawing some stuff on death

--- src/views/GameOverView.ts
+++ src/views/GameOverView.ts
@@ -3,6 +3,11 @@ class GameOverView extends BaseView {
         super();
     }
     public update() {}
-    public drawGUI() {}
+    
+    public drawGUI() {
+        this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y-50), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y+50), "black");
+        this.canvasHelper.writeText("You died!", 96, this.canvasHelper.getCenter(), undefined, undefined, "red");
+    }
+
     public beforeExit() {}
 }
