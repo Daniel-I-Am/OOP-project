c8eaa4db3ab3b2c0132c9aee3c5d86dbf2f82698
Daniel de Cloet <djdecloet@gmail.com>
2018-12-17 11:17:20 +0100

    Death Screen

--- build/app.js
+++ build/app.js
@@ -684,12 +684,16 @@ class SoundHelper {
 class GameOverView extends BaseView {
     constructor() {
         super();
-    }
-    update() { }
-    drawGUI() {
-        this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y - 50), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y + 50), "black");
+        this.shouldClear = false;
+        for (let i = 0; i < 85; i++) {
+            this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y - 50 - i), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y - 51 - i), `rgba(0, 0, 0, ${(85 - i) / 100})`);
+            this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y + 51 + i), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y + 50 + i), `rgba(0, 0, 0, ${(85 - i) / 100})`);
+        }
+        this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y - 50), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y + 50), `rgba(0, 0, 0, ${85 / 100})`);
         this.canvasHelper.writeText("You died!", 96, this.canvasHelper.getCenter(), undefined, undefined, "red");
     }
+    update() { }
+    drawGUI() { }
     beforeExit() { }
 }
 //# sourceMappingURL=app.js.map
