2ee2a1b5ce4055d0e101b5a9b8756d519b00baa8
AngryAshley <verb0078@hz.nl>
2018-12-19 09:09:35 +0100

    Made the game-over message more pegi-7

--- src/views/GameOverView.ts
+++ src/views/GameOverView.ts
@@ -15,7 +15,7 @@ class GameOverView extends BaseView {
             this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y+ 51 + i), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y + 50 + i), `rgba(0, 0, 0, ${(85-i)/100})`);
         }
         this.canvasHelper.fillRect(new Vector(0, this.canvasHelper.getCenter().y-50), new Vector(this.canvasHelper.getWidth(), this.canvasHelper.getCenter().y+50), `rgba(0, 0, 0, ${85/100})`);
-        this.canvasHelper.writeText("You died!", 96, this.canvasHelper.getCenter(), undefined, undefined, "red");
+        this.canvasHelper.writeText("Game over!", 96, this.canvasHelper.getCenter(), undefined, undefined, "red");
     }
 
     public beforeExit() {}
