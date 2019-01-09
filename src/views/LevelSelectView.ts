c1c1d899c5a5ad62136a2e40188bd775139994c8
AngryAshley <verb0078@hz.nl>
2019-01-09 08:32:00 +0100

    Fixed Level1.jason

--- src/views/LevelSelectView.ts
+++ src/views/LevelSelectView.ts
@@ -3,6 +3,7 @@ class LevelSelectView extends BaseView {
     public constructor() {
         super();
         this.backgroundMusic = new SoundHelper("./assets/sounds/Pulsewave.wav", .3);
+        this.backgroundMusic.toggleLoop();
         this.background = new Image();
         this.background.src = "./assets/images/level_select.png";
         this.entities = [];
